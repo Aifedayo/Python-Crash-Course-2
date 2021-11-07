d9c7acb3edd2e5d2dc38953477ef3e679f08268a
Aifedayo <akeemifedayolag@gmail.com>
2021-11-07 11:52:44 +0100

    random_player_selection

--- random_choice.py
+++ random_choice.py
@@ -2,10 +2,11 @@ import random, time
 
 
 team_mates = ['mushrah', 'noyad', 'salami', 'codedbashir', 'thesmathub', 'kaybee', 'zee', 'samad', 'salakoa','sulayman']
-present_team_mates = ['mushrah', 'zee', 'noyad', 'codedbashir']
+present_team_mates = ['mushrah', 'zee', 'noyad', 'codedbashir', 'thesmathub', 'samad', 'salakoA', 'sulayman', 'kaybee']
 
 if len(present_team_mates) < len(team_mates):
     team_mate = random.sample(present_team_mates, k=(len(present_team_mates)))
+
 
 else:
     team_mate = random.sample(team_mates, k=len(team_mates))
@@ -15,8 +16,8 @@ def python_class(*args):
         content = ''.join(my_question.readlines()).split('\n')
         questions = random.sample(content, len(team_mate))
     
-    index_list = ['[4:15]', '[::2]', '[:10]', '[3:12]', '[:10]', '[::]', '[10:]']
-    index_list = random.sample(index_list, len(team_mate))
+    # index_list = ['[-4:15]', '[::2]', '[:10]', '[3:12]', '[:10]', '[::]', '[10:]']
+    # index_list = random.sample(index_list, len(team_mate))
     
     print("Using the given index, what will each of the statement return?")
     names = [name for name in team_mate]
@@ -24,9 +25,8 @@ def python_class(*args):
     quests = [question for question in questions]
     
 
-    for item in zip(names, quests, index_list):
+    for item in zip(names, quests):
         print(item)
 
 
-    
 #python_class(team_mate)
